69ea1f69ef947915bbde724d33067ba7609eaf3c
Akshay Modak <akshaymodak127@gmail.com>
2017-09-06 23:16:06 +0530

    Added support to drag dialog box and made movie trailer dialog box draggable.

--- src/main/webapp/javascript/angular-directives.js
+++ src/main/webapp/javascript/angular-directives.js
@@ -74,3 +74,49 @@ myApp.directive('slideable', function () {
         }
     }
 });
+
+
+// reference -- http://embed.plnkr.co/NQihUs/
+// Improve it -- something like this -- http://embed.plnkr.co/8CHoiN/preview
+myApp.directive('modalMovable', ['$document',
+    function($document) {
+        return {
+            restrict: 'AC',
+            link: function(scope, iElement, iAttrs) {
+                var startX = 0,
+                    startY = 0,
+                    x = 0,
+                    y = 0;
+
+                var dialogWrapper = iElement.parent();
+
+                dialogWrapper.css({
+                    position: 'relative'
+                });
+
+                dialogWrapper.on('mousedown', function(event) {
+                    // Prevent default dragging of selected content
+                    event.preventDefault();
+                    startX = event.pageX - x;
+                    startY = event.pageY - y;
+                    $document.on('mousemove', mousemove);
+                    $document.on('mouseup', mouseup);
+                });
+
+                function mousemove(event) {
+                    y = event.pageY - startY;
+                    x = event.pageX - startX;
+                    dialogWrapper.css({
+                        top: y + 'px',
+                        left: x + 'px'
+                    });
+                }
+
+                function mouseup() {
+                    $document.unbind('mousemove', mousemove);
+                    $document.unbind('mouseup', mouseup);
+                }
+            }
+        };
+    }
+]);

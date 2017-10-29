d987f1e0f231079248fc794fca9e9d084482c80c
Akshay Modak <akshaymodak127@gmail.com>
2017-10-29 19:19:27 +0530

    Updated GeoLite API and it's respective code logic.

--- src/main/java/one/Visit.java
+++ src/main/java/one/Visit.java
@@ -10,12 +10,13 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
-import com.maxmind.geoip.Location;
-import com.maxmind.geoip.LookupService;
-import com.maxmind.geoip.regionName;
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
 
 //reference -- https://www.mkyong.com/java/java-find-location-using-ip-address/
 //api -- https://github.com/maxmind/geoip-api-java
@@ -23,24 +24,10 @@ public class Visit {
 
     public void setVisit(HttpServletRequest request) {
         String nextrr_home = System.getProperty("user.dir");
-        String geoDataPath = nextrr_home + "/ext-data/geoData/GeoLiteCity.dat";
-
-        File file = new File(geoDataPath);
-        /* String ipAddressRequestCameFrom = request.getRemoteAddr(); */
+        String geoDataPath = nextrr_home + "/ext-data/geoData/GeoLite2-City.mmdb";
 
         String ipAddressRequestCameFrom = request.getHeader("x-forwarded-for");
-        System.out.println("====ipAddressRequestCameFrom====" + ipAddressRequestCameFrom);
-
         Map<String, String> result = new HashMap<>();
-
-        Enumeration headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String key = (String) headerNames.nextElement();
-            String value = request.getHeader(key);
-            result.put(key, value);
-        }
-
-        System.out.println("====result====" + result);
 
         if (ipAddressRequestCameFrom == null) {
             ipAddressRequestCameFrom = request.getRemoteAddr();
@@ -48,24 +35,21 @@ public class Visit {
         String ipAddressesRequestCameFrom[] = ipAddressRequestCameFrom.split(",");
         ipAddressRequestCameFrom = ipAddressesRequestCameFrom[0];
 
-        System.out.println("====ipAddressRequestCameFrom====" + ipAddressRequestCameFrom);
-
-        ServerLocation location = null;
+        Map<String, Object> location = null;
         Calendar cal = Calendar.getInstance();
-        DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        DateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         String requestDate = sdf.format(cal.getTime());
+        sdf = new SimpleDateFormat("hh:mm a");
         String requestTime = sdf.format(cal.getTime());
 
         String requestUri = request.getRequestURI();
-        try {
-            location = getLocation(ipAddressRequestCameFrom, file);
-        } catch (NullPointerException npe) {
-            System.out.println("====Visit Location is Null=====" + npe);
+        location = getLocation(ipAddressRequestCameFrom, geoDataPath);
+        if (location.isEmpty()) {
+            System.out.println("ERROR: Visit Location is Null");
             return;
         }
         if (location != null) {
             DatabaseUtils dbUtils = new DatabaseUtils();
-
             Map<String, Object> queryParams = new HashMap<String, Object>();
             queryParams.put("requestDate", requestDate);
             queryParams.put("userIp", ipAddressRequestCameFrom);
@@ -76,49 +60,38 @@ public class Visit {
 
             if (resultList.isEmpty()) {
                 Map<String, Object> queryMap = new HashMap<String, Object>();
-
                 queryMap.put("requestUri", requestUri);
                 queryMap.put("userIp", ipAddressRequestCameFrom);
-                queryMap.put("userCity", location.getCity());
-                queryMap.put("userCountry", location.getCountryName());
+                queryMap.put("userCity", location.get("cityName"));
+                queryMap.put("userCountry", location.get("countryName"));
                 queryMap.put("requestDate", requestDate);
                 queryMap.put("requestTime", requestTime);
 
                 dbUtils.runCreateQuery("visit", queryMap);
-
             } else {
-                System.out.println("===Visit Already Exists===");
+                System.out.println("ALERT: Visit Already Exists");
             }
         }
     }
 
-    public ServerLocation getLocation(String ipAddress, File file) {
-
-        ServerLocation serverLocation = null;
+    public Map<String, Object> getLocation(String ip, String dbLocation) {
+        Map<String, Object> geoMap = new HashMap<String, Object>();
 
         try {
+            File database = new File(dbLocation);
+            DatabaseReader dbReader = new DatabaseReader.Builder(database).build();
 
-            serverLocation = new ServerLocation();
+            InetAddress ipAddress = InetAddress.getByName(ip);
+            CityResponse response = dbReader.city(ipAddress);
 
-            LookupService lookup = new LookupService(file, LookupService.GEOIP_MEMORY_CACHE);
-            Location locationServices = lookup.getLocation(ipAddress);
+            geoMap.put("countryName", response.getCountry().getName());
+            geoMap.put("stateName", response.getLeastSpecificSubdivision().getName());
+            geoMap.put("cityName", response.getCity().getName());
+            geoMap.put("postalCode", response.getPostal().getCode());
 
-            serverLocation.setCountryCode(locationServices.countryCode);
-            serverLocation.setCountryName(locationServices.countryName);
-            serverLocation.setRegion(locationServices.region);
-            serverLocation
-                    .setRegionName(regionName.regionNameByCode(locationServices.countryCode, locationServices.region));
-            serverLocation.setCity(locationServices.city);
-            serverLocation.setPostalCode(locationServices.postalCode);
-            serverLocation.setLatitude(String.valueOf(locationServices.latitude));
-            serverLocation.setLongitude(String.valueOf(locationServices.longitude));
-
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
+        } catch (IOException | GeoIp2Exception e) {
+            System.out.println("ERROR: Error while getting location with IP====" + e);
         }
-
-        return serverLocation;
-
+        return geoMap;
     }
-
 }

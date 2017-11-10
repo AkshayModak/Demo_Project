9a487c2776191f7e218351895e93b68c9f9c876a
Akshay Modak <akshaymodak127@gmail.com>
2017-11-10 23:43:59 +0530

    Done Code formatting.

--- src/main/java/one/ReadXMLFile.java
+++ src/main/java/one/ReadXMLFile.java
@@ -13,49 +13,49 @@ import org.w3c.dom.NodeList;
 
 public class ReadXMLFile {
 
-	public static Map<String, Object> getXMLData(String fileName) {
+    public static Map<String, Object> getXMLData(String fileName) {
 
-		Map<String, Object> nodeResult = new HashMap<String, Object>();
-		try {
-			
-	    	File file = new File(fileName);
-			DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			Document doc = dBuilder.parse(file);
-			if (doc.hasChildNodes()) {
-				nodeResult = printNote(doc.getChildNodes());
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		
-		return nodeResult;
+        Map<String, Object> nodeResult = new HashMap<String, Object>();
+        try {
 
-	}
+            File file = new File(fileName);
+            DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+            if (doc.hasChildNodes()) {
+                nodeResult = printNote(doc.getChildNodes());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-	private static Map<String, Object> printNote(NodeList nodeList) {
+        return nodeResult;
 
-		Map<String, Object> nodeData = new HashMap<String, Object>();
-		String nodeName = null;
-		String nodeValue = null;
-		
-		for (int count = 0; count < nodeList.getLength(); count++) {
-			Node tempNode = nodeList.item(count);
-			if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
-				if (tempNode.hasAttributes()) {
-					NamedNodeMap nodeMap = tempNode.getAttributes();
-					for (int i = 0; i < nodeMap.getLength(); i++) {
-						Node node = nodeMap.item(i);
-						nodeName = node.getNodeName();
-						nodeValue = node.getNodeValue();
-						nodeData.put(nodeName, nodeValue);
-					}
-				}
-				if (tempNode.hasChildNodes()) {
-					return printNote(tempNode.getChildNodes());
-				}
-			}
-		}
-		return nodeData;
-	}
+    }
+
+    private static Map<String, Object> printNote(NodeList nodeList) {
+
+        Map<String, Object> nodeData = new HashMap<String, Object>();
+        String nodeName = null;
+        String nodeValue = null;
+
+        for (int count = 0; count < nodeList.getLength(); count++) {
+            Node tempNode = nodeList.item(count);
+            if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+                if (tempNode.hasAttributes()) {
+                    NamedNodeMap nodeMap = tempNode.getAttributes();
+                    for (int i = 0; i < nodeMap.getLength(); i++) {
+                        Node node = nodeMap.item(i);
+                        nodeName = node.getNodeName();
+                        nodeValue = node.getNodeValue();
+                        nodeData.put(nodeName, nodeValue);
+                    }
+                }
+                if (tempNode.hasChildNodes()) {
+                    return printNote(tempNode.getChildNodes());
+                }
+            }
+        }
+        return nodeData;
+    }
 
 }
